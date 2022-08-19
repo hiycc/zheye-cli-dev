f61e7535e08919ecf260933482c7433fcb323170
hiycc <hiycc@outlook.com>
2022-08-19 14:30:16 +0800

    利用node多进程动态执行命令

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const cp = require('child_process')
 const log = require('@zheye-cli-dev/log')
 const Package = require('@zheye-cli-dev/package')
 const path = require('path')
@@ -22,7 +23,7 @@ async function exec() {
   const cmdName = cmdObj.name()
   const packageName = SETTINGS[cmdName]
   const packageVersion = '1.1.0'
-  if(!targetPath) {
+  if (!targetPath) {
     targetPath = path.resolve(homePath, CACHE_DIR)
     storeDir = path.resolve(targetPath, 'node_modules')
     log.verbose('targetPath', targetPath)
@@ -53,9 +54,34 @@ async function exec() {
   // console.log('arguments', arguments)
   if (rootFile) {
     try {
-      require(rootFile).call(null, Array.from(arguments))
-    }
-    catch(e) {
+      
+      //  在node子进程进行调用
+      const args = Array.from(arguments)
+      const cmd = args[args.length - 1]
+      const o = Object.create(null)
+      Object.keys(cmd).forEach(key => {
+        if (cmd.hasOwnProperty(key) &&
+          !key.startsWith('_') &&
+          key !== 'parent' 
+        ) {
+          o[key] = cmd[key]
+        }
+      })
+      args[args.length - 1] = o
+      const code = `require('${rootFile}').call(null, ${JSON.stringify(args)})`
+      const child = cp.spawn('node', ['-e', code], {
+        cwd: process.cwd(),
+        stdio: 'inherit'
+      })
+      child.on('error', e => {
+        log.error(e.message)
+        process.exit(1)
+      })
+      child.on('exit', e => {
+        log.verbose('命令执行成功' + e)
+        process.exit(e)
+      })
+    } catch (e) {
       log.error(e.message)
     }
   }
